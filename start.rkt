ee6b0d7fca6a1e7eae206b5e594722332c250b67
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-10-20 23:36:54 +0200

    Wow meme

--- start.rkt
+++ start.rkt
@@ -1,7 +1,21 @@
 #lang scheme
-(define cal1 (list "mah name"))
 (define cal2 (list "sw7" (list (list "PP" 1 2) (list "CC" 6 7) (list "Pause" 69 1337) (list "Secret Calendar" (list (list "Secret PP" 1 2) (list "Secret CC" 6 7) (list "Secret Pause" 69 1337))) (list "Meme time" 5 2))))
 (define app1 (list "some text" 213123123 1222223))
+
+(define (calendar title [content null])
+  (cond
+    [((negate string?) title) (error "Title must be provided")]
+    [(null? content) (list title '("No"))]
+    [(list? content)
+     (list title (filter (lambda (x)
+                           (or (appointment? x) (calendar? x ))) content))]
+    [else (error "Content can only be list if supplied")]))
+
+(define (appointment title from to)
+  (let ((ap (list title from to)))
+    (match ap
+      [(list (? string? ti) (? number? fr) (? (lambda (x) (and (number? x) (> x from))) to)) (list ti fr to)]
+      [_ (error "Not valid appointment")])))
 
 (define ++ add1)
 (define -- sub1)
@@ -24,6 +38,16 @@
          (cals (filter calendar? content)))
     (append (filter appointment? content) (apply append (map extract-appointments cals)))))
 
+(define (extract-fromtimestamp ap)
+  (cond
+    [(appointment? ap) (list-ref ap 1)]
+    [else error("Not an appointment")]))
+
+(define (extract-totimestamp ap)
+  (cond
+    [(appointment? ap) (list-ref ap 2)]
+    [else error("Not an appointment")]))
+
 (define (appointment? x)
   (match x
     [(list (? string?) (? number? from) (? number? to)) (< from to)]
@@ -31,23 +55,46 @@
 
 (define (calendar? x)
   (match x
-    [(list (? string?) (? list?)) #t]
+    [(list (? string?) (or (? empty?) (list (or (? calendar?) (? appointment?)) ... ))) #t]
     [_ #f]))
 
 (define (flatten-calendar cal)
   (cond
-    (((negate calendar?) cal) (error "That is not a calendar"))
-    (else
-     (list (extract-title cal) (extract-appointments cal)))))
+    [(calendar? cal) (list (extract-title cal) (extract-appointments cal))]
+    [else (error "That is not a calendar")]))
 
 (define (add-appointment cal appointment)
   (cond
-    ((null? cal) (error "FAK"))
-    ((empty? appointment) (error "FAK"))
-    ((empty? (cdr cal)) (list (car cal) (cons appointment '())))
-    ((list? (cdr cal)) (list (car cal) (cons appointment (extract-content cal))))
-    (else (error "FAK"))))
+    [(null? cal) (error "FAK")]
+    [(empty? appointment) (error "FAK")]
+    [(empty? (extract-content cal)) (calendar (extract-title cal) (list appointment))]
+    [(list? (extract-content cal)) (calendar (extract-title cal) (append (extract-content cal) (list appointment)))]
+    [else (error "FAK")]))
 
+(define (add-calendar cal-a cal-b)
+  (cond
+    [(null? cal-a) (error "FAK")]
+    [(empty? appointment) (error "FAK")]
+    [(empty? (extract-content cal-a)) (calendar (extract-title cal-a) (list cal-b))]
+    [(list? (extract-content cal-a)) (calendar (extract-title cal-a) (append (extract-content cal-a) (list cal-b)))]
+    [else (error "FAK")]))
+
+(define (find-appointments cal pred)
+  (filter pred (extract-appointments cal)))
+
+(define (find-first-helper list pred)
+  (let loop ([l list])
+    (cond
+      [(null? l) #f]
+      [else (let ([a (car l)])
+              (if (pred a) a (loop (cdr l))))])))
+
+(define (find-first-appointment cal pred)
+  (find-first-helper (extract-appointments cal) pred))
+
+(define (find-last-appointment cal pred)
+  (find-first-helper (reverse (extract-appointments cal)) pred))
+  
 ;;HELPER FUNCTIONS
 (define range (lambda (a [b null] [step 1])
   (cond
@@ -122,26 +169,23 @@
   (format "~A/~A/~A ~A | ~A:~A:~A" (year ts) (month ts) (day-in-month ts) (name-of-day ts) (hour ts) (minute ts) (second ts)))
 
 
-;(add-appointment cal1 app1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   TEST   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;(add-appointment (add-appointment cal1 app1) app1)
-
-;(add-appointment (add-appointment cal1 app1) (list "memes" 123 456))
-
-;(extract-content (add-appointment (add-appointment cal1 app1) (list "memes" 123 456)))
-;(display "FILTER\n")
-;(filter appointment? (extract-content (add-appointment (add-appointment cal1 app1) (list "memes" 123 456))))
-
-;(filter appointment? (extract-content cal2))
-;(filter calendar? (extract-content cal2))
-
-(flatten-calendar cal2)
-cal2
-(current-seconds)
+(define cal1 (calendar "Pepe" (list (appointment "yay" 22 33) (calendar "Lol" (list (appointment "wat" 123 292) (appointment "nay" 456 788))))))
+cal1
+(flatten-calendar cal1)
 (timestamp->civil-date (current-seconds))
-(timestamp->civil-date 11847456550)
-(map timestamp->civil-date (range (current-seconds) (+ (current-seconds) (* SECS_IN_A_DAY 14)) SECS_IN_A_DAY))
+;(map timestamp->civil-date (range (current-seconds) (+ (current-seconds) (* SECS_IN_A_DAY 14)) SECS_IN_A_DAY))
 
+(define (fak? x)
+  (match x
+    [(list (? string?) (or (? empty?) (list (or (? string?) (? number?)) ... ))) #t]
+    [_ #f]))
 
+(calendar? cal1)
+(calendar? (add-appointment cal1 (appointment "WOW" 23 24)))
+(appointment? (appointment "yayaayayayay" 22 55))
+(add-appointment cal1 (appointment "WOW" 23 24))
 
-
+(find-first-appointment cal1 (lambda (a) (if (> (extract-fromtimestamp a) 20) #t #f)))
+(find-last-appointment cal1 (lambda (a) (if (> (extract-fromtimestamp a) 20) #t #f)))
