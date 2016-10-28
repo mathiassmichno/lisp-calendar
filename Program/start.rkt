78b75d527cf95ac333fbb87dbeccaa734a4e43ed
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-10-28 16:19:02 +0200

    final commit before turning in

--- Program/start.rkt
+++ Program/start.rkt
@@ -5,11 +5,8 @@
 ; Name:    Mathias Sass Michno     ;
 ; StudyNo. 20130250                ;
 ; E-Mail:  mmichn13@student.aau.dk ;
+; Date:    28/10/2016              ;
 ;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define cal2 (list "sw7" (list (list "PP" 22 33) (list "CC" 66 77) (list "Pause" 69 1337) (list "Secret Calendar" (list (list "Secret PP" 11 22) (list "Secret CC" 13 14) (list "Secret Pause" 69 1337))) (list "Meme time" 21 22))))
-(define app1 (list "some text" 213123123 1222223))
 
 ; Calendar constructor function. Can be called with or without content
 (define (calendar title [content '()])
@@ -271,38 +268,29 @@
 
 ;;TIMESTAMPS
 
-; Number of seconds in a day
 (define SECS_IN_A_DAY 86400)
 
-; Calculate which seconds a given timestamp ts is in the day
 (define (second-in-day ts)
   (modulo ts SECS_IN_A_DAY))
 
-; Calculate the second of a timestamp ts
 (define (second ts)
   (modulo (second-in-day ts) 60))
 
-; Calculate the minute of a timestamp ts
 (define (minute ts)
   (modulo (quotient (second-in-day ts) 60) 60))
 
-; Calculate the hour of a timestamp ts
 (define (hour ts)
   (modulo (quotient (second-in-day ts) 3600) 24))
 
-; Calculate which day since january 1st 1940 ts represents
 (define (day-in-forever ts) ;forever is a timeperiod from january 1st 1970 to the end of all time
   (quotient ts SECS_IN_A_DAY))
 
-; Calculate which day a timestamp ts represents in the week
 (define (day-in-week ts)
   (modulo (+ (day-in-forever ts) 3) 7))
 
-; Output the name of a given weekday relative to the timestamp ts
 (define (name-of-day ts)
   (list-ref '("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun") (day-in-week ts)))
 
-;
 (define (days-in-month month year)
   (let ((month (sub1 month)))
     (cond
@@ -320,7 +308,7 @@
 
 (define (month ts)
   (cdr (month-day-helper (day-in-year ts) 1 (year ts))))
-  
+
 (define (leap-year? year)
   (cond
     [((negate number?) year) (error "Year provided not a number")]
@@ -328,7 +316,7 @@
     [(eq? (modulo year 100) 0) #f]
     [(eq? (modulo year 4) 0) #t]
     [else #f]))
-
+ 
 (define (day-year-helper day year)
   (let ((year-length (if (leap-year? year) 366 365)))
     (cond
@@ -380,18 +368,22 @@
              (timestamp->civil-time (totimestamp appointment)))])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HTML ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Present a calendar cal as html by writing it to a file with the same name as the calendar
+; The presented calendar will span from from-time to to-time
 (define (present-calendar-html cal from-time to-time)
   (let ((c (calendar (title cal) (filter (lambda (x)
                                            (appointments-overlap? x (appointment "fake" from-time to-time)))
                                          (appointments cal)))))
   (write-to-file (format "~A.html" (title cal)) (html-document (title cal) (agenda c)))))
 
+; Generate a html agenda for a given calendar
 (define (agenda cal)
   (let* ([aps (appointments cal)]
          [cal-start (fromtimestamp (first aps))]
          [cal-end (totimestamp (first (sort aps ends-after?)))])
     (format
-     "<h1><marquee>~A</marquee></h1>~n~A"
+     "<h1>~A</h1>~n~A"
      (title cal)
      (string-join
       (filter (lambda (s) (string-contains? s "<li>"))
@@ -411,6 +403,7 @@
                (days-in-range cal-start cal-end)))
       "\n"))))
 
+; Generate a valid html document with a given title and optional content for the body
 (define (html-document title [body-content null])
   (let ([html_string (format "<!doctype html>~n<title>~A</title>~n" title)])
     (if (null? body-content)
@@ -419,68 +412,18 @@
                                        (format "<body>~n~A~n</body>" body-content)
                                        (format "<body>~n~A~n</body>" (string-append* body-content)))))))
 
+; Write content to file with filename.
+; Will replace if the file aldready exists
 (define (write-to-file filename content)
   (with-output-to-file filename (lambda () (display content)) #:exists 'replace))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   TEST   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Load test cal into namespace
 (define-namespace-anchor anc)
 (define ns (namespace-anchor->namespace anc))
 (define test-cal (eval (call-with-input-file "test_calendar" read) ns))
 
-(define cal1 (calendar "Pepe" (list (appointment "yay" 1 11) (calendar "Lol" (list (appointment "wat" 3 4) (appointment "nay" 6 8))))))
-(timestamp->civil-date (current-seconds))
-
-(calendar? cal1)
-(calendar? (add-appointment cal1 (appointment "WOW" 23 24)))
-(calendar? (add-calendar cal1 (calendar "WOW" (list (appointment "meme" 4 20) (appointment "dank" 1 2)))))
-(define da-cal (add-calendar cal1 (calendar "WOW" (list (appointment "meme" 4 20) (appointment "dank" 1 2))))) 
-"t"
-(appointment? (appointment "yayaayayayay" 22 55))
-(add-appointment cal1 (appointment "WOW" 23 24))
-
-(find-first-appointment cal1 (lambda (a) (if (> (fromtimestamp a) 2) #t #f)))
-(find-last-appointment cal1 (lambda (a) (if (> (fromtimestamp a) 20) #t #f)))
-
-(calendars-overlap? cal1 cal2)
+; Generate test html
 (present-calendar-html test-cal 1475280000 1477958399)
-(write-to-file "test.html" (html-document (title test-cal) (agenda test-cal)))
-(define oct-a (range-in-month (current-seconds)))
-(define oct-b (range-in-month 1477958399))
-(timestamp->civil-date (list-ref oct-a 0))
-(timestamp->civil-date (list-ref oct-b 0))
-(timestamp->civil-date (last oct-a))
-(timestamp->civil-date (last oct-b))
-
-"TESTING RANGE IN MONTH FUNC"
-(define test-range (range 1475280000 1477958399 1337))
-(define oct-days (days-in-range 1475280666 1477957666))
-(define oct-ulti (map (lambda (x) (list x (range-in-month x))) test-range))
-(map (lambda(a)
-   (format "~A  ~A ~A  ~A  ~A"
-           (timestamp->civil (first a))
-           (days-in-month (month (first a)) (year (first a)))
-           (length (last a))
-           (map timestamp->civil (start-and-end-of-month (first a)))
-           (map timestamp->civil (list (first (last a)) (last (last a)))))) (filter (lambda (x) (= 1477871999 (last (last x)))) oct-ulti))
-
-"TESTING START AND END OF MONTH FUNC"
-(define wat-ulti (map (lambda (x) (list x (start-and-end-of-month x))) (range 1475280000 1477958399 (* 30 60))))
-(map
- (lambda(a)
-   (format "~A    ~A"
-           (timestamp->civil (first a))
-           (map timestamp->civil (last a))))
- (filter
-  (lambda (x)
-    (let ((x (last x)))
-      (not
-       (and (and (>= (first x) 1475280000) (<= (first x) 1475366399))
-            (and (>= (last x) 1477872000) (<= (last x) 1477958399))))))
-  wat-ulti))
-
-(calendar? (add-calendar cal1 (calendar "meem")))
-(flatten-calendar test-cal)
-(remove-appointments (add-calendar test-cal cal1) (lambda (a) (string-contains? (title a) "ay")))
-(remove-from-calendar (add-calendar test-cal cal1) appointment? (lambda (a) (string-contains? (title a) "ay")))
